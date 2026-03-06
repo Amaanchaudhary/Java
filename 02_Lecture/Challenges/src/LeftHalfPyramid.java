8a6df2b31c6542c878d3bd7081767608386c558d
Amaan Chaudhary <chaudharyamaan982@gmail.com>
2026-03-06 16:39:58 +0530

    2:10:00 lecture 2 Challenges Completed

---
+++ 02_Lecture/Challenges/src/LeftHalfPyramid.java
@@ -0,0 +1,9 @@
+public class LeftHalfPyramid {
+    static void main(String[] args) {
+        System.out.println("        *");
+        System.out.println("      * *");
+        System.out.println("    * * *");
+        System.out.println("  * * * *");
+        System.out.println("* * * * *");
+    }
+}
---
+++ 02_Lecture/Challenges/src/ReverseRightHalfPyramid.java
@@ -0,0 +1,9 @@
+public class ReverseRightHalfPyramid {
+    static void main(String[] args) {
+        System.out.println("* * * * *");
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*");
+    }
+}
