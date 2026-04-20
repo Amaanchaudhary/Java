e3936e4e50f17d210b81f46961d0315735815c30
Amaan Chaudhary <chaudharyamaan982@gmail.com>
2026-04-20 18:30:56 +0530

    06:43:00 Learning Need of Multithreading with example

---
+++ 12 Multi Threading and Executor Service/Lecture 12/src/in/kgcoding/multithreading/NeedOfMultiThreading.java
@@ -0,0 +1,24 @@
+package in.kgcoding.multithreading;
+
+public class NeedOfMultiThreading {
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        for (int i = 1; i <= 10000; i++) {
+            System.out.printf("%d* ", i);
+        }
+        System.out.println("\nStar Task Completed");
+
+        for (int i = 1; i <= 10000; i++) {
+            System.out.printf("%d$ ", i);
+        }
+        System.out.println("\nDollar Task Completed");
+
+        for (int i = 1; i <= 10000; i++) {
+            System.out.printf("%d# ", i);
+        }
+        System.out.println("\nHash Task Completed");
+        long end = System.currentTimeMillis();
+        System.out.printf("total time taken %d ms", (end - start));
+
+    }
+}
